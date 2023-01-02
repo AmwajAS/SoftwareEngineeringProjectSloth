cc9eb017643578dfa11bdc5370ca441253b37d4b
HabebNawatha <habeb.naw@outlook.com>
2023-01-02 12:43:28 +0200

    Question cell mark added.

--- src/Model/QuesCell.java
+++ src/Model/QuesCell.java
@@ -6,6 +6,10 @@ import java.util.Optional;
 import java.util.Random;
 
 import Controller.QuestionPopupController;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.BorderStrokeStyle;
@@ -15,6 +19,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -22,19 +27,19 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
 
 public class QuesCell extends Cell{
 	private Question question;
-	
+	final String image = "/images/questionMark.png";
 	public QuesCell(int x, int y, Question q, Game g) {
 		super(x, y);
 		this.question=q;
-;
 	}
 
 	public QuesCell(int x, int y) {
 		super(x, y);
-		setStyle("-fx-background-color: red;");
+		this.setStyle("-fx-background-image: url('"+image+"'); -fx-background-size: 100px 100px; -fx-background-position: center center; -fx-background-repeat:no-repeat;");
 	}
 
 	public Question getQuestion() {
